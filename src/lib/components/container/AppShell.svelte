44a8cad85632688ab9acf89f13be80e61addda8e
Ezekiel Victor <codeepoch@gmail.com>
2023-08-16 07:31:22 +0100

    updated AppShell layout

--- src/lib/components/container/AppShell.svelte
+++ src/lib/components/container/AppShell.svelte
@@ -2,7 +2,7 @@
   <div class="absolute w-full z-20 top-0 left-0 border-b">
     <slot name="nav" />
   </div>
-  <div class="mt-7">
+  <div class="mt-20 md:mt-7">
     <slot />
   </div>
   <div>
--- src/lib/components/visitors/Footer.svelte
+++ src/lib/components/visitors/Footer.svelte
@@ -4,24 +4,26 @@
 	import Logo from "../constants/Logo.svelte";
 
 </script>
-<Footer footerType="logo">
-  <div class="flex items-center justify-between pb-5">
-    <a href="/#">
-      <Logo />
-    </a>
-    <FooterLinkGroup ulClass="flex  flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0">
-      <FooterLink href="/#">About</FooterLink>
-      <FooterLink href="/#">Services</FooterLink>
-      <FooterLink href="/#">Pricing</FooterLink>
-      <FooterLink href="/#">Contact</FooterLink>
-    </FooterLinkGroup>
-  </div>
-  <div class="border-t pt-5 flex items-center justify-between">
-    <FooterCopyright href="/" by="Aquamark™" />
-    <div>
-      <FooterIcon href="https://www.linkedin.com/company/aquamark-integrated-services/">
-        <Icon icon="mdi:linkedin" class="text-xl hover:text-blue-600 transition"/>
-      </FooterIcon>
+<div class="mt-10">
+  <Footer footerType="logo">
+    <div class="flex flex-col md:flex-row gap-4 justify-between md:pb-5">
+      <a href="/#">
+        <Logo />
+      </a>
+      <FooterLinkGroup ulClass="flex  flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0">
+        <FooterLink href="/#">About</FooterLink>
+        <FooterLink href="/#">Services</FooterLink>
+        <FooterLink href="/#">Pricing</FooterLink>
+        <FooterLink href="/#">Contact</FooterLink>
+      </FooterLinkGroup>
     </div>
-  </div>
-</Footer>
+    <div class="border-t pt-5 flex items-center justify-between">
+      <FooterCopyright href="/" by="Aquamark™" />
+      <div>
+        <FooterIcon href="https://www.linkedin.com/company/aquamark-integrated-services/">
+          <Icon icon="mdi:linkedin" class="text-xl hover:text-blue-600 transition"/>
+        </FooterIcon>
+      </div>
+    </div>
+  </Footer>
+</div>
